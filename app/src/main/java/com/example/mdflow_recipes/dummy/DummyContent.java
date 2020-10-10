513844377842f0acaef6106102154e12597f9c1f
amiel <amiel.castillo16@gmail.com>
2020-10-10 16:08:48 -0400

    Splash not working for tablet mode, commented out launcher attribute

--- app/src/main/java/com/example/mdflow_recipes/dummy/DummyContent.java
+++ app/src/main/java/com/example/mdflow_recipes/dummy/DummyContent.java
@@ -1,5 +1,7 @@
 package com.example.mdflow_recipes.dummy;
 
+import com.example.mdflow_recipes.R;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,7 +14,7 @@ import java.util.Map;
  * TODO: Replace all uses of this class before publishing your app.
  */
 public class DummyContent {
-
+    String[] recipeTitles, recipeDescriptions;
     /**
      * An array of sample (dummy) items.
      */
@@ -23,20 +25,27 @@ public class DummyContent {
      */
     public static final Map<String, DummyItem> ITEM_MAP = new HashMap<String, DummyItem>();
 
-    private static final int COUNT = 25;
+    //private static final int COUNT = 25;
 
     static {
+        addItem(new DummyItem("1", "Bacon & Spinach Stuffed Chicken"));
+        addItem(new DummyItem("2", "Tex-Mex Meatballs"));
+        addItem(new DummyItem("3", "Best-Ever Calzones"));
+        addItem(new DummyItem("4", "Baked Spaghetti"));
+        addItem(new DummyItem("5", "Cheesy Ground Beef Tacos"));
+        addItem(new DummyItem("6", "Beef Totchos"));
+
         // Add some sample items.
-        for (int i = 1; i <= COUNT; i++) {
-            addItem(createDummyItem(i));
-        }
+        //for (int i = 1; i <= COUNT; i++) {
+        //    addItem(createDummyItem(i));
+        //}
     }
 
     private static void addItem(DummyItem item) {
         ITEMS.add(item);
         ITEM_MAP.put(item.id, item);
     }
-
+/*
     private static DummyItem createDummyItem(int position) {
         return new DummyItem(String.valueOf(position), "Item " + position, makeDetails(position));
     }
@@ -49,19 +58,26 @@ public class DummyContent {
         }
         return builder.toString();
     }
-
+*/
     /**
      * A dummy item representing a piece of content.
      */
     public static class DummyItem {
         public final String id;
-        public final String content;
-        public final String details;
+        public String content;
+        public String recipeTitle;
+        public String recipeDescription;
+        public int images[];
 
-        public DummyItem(String id, String content, String details) {
+        public DummyItem(String id, String content) {
             this.id = id;
             this.content = content;
-            this.details = details;
+        }
+        public DummyItem(String id, String recipeTitle, String recipeDesc) {
+            this.id = id;
+            content = recipeTitle;
+            this.recipeTitle = recipeTitle;
+            recipeDescription = recipeDesc;
         }
 
         @Override
