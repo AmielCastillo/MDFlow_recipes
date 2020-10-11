4128464385ae63be38ec84e69104b9e4dfc61027
amiel <amiel.castillo16@gmail.com>
2020-10-11 18:05:16 -0400

    Completed, no sound on tablet but might be local audio issue

--- app/src/main/java/com/example/mdflow_recipes/ItemDetailFragment.java
+++ app/src/main/java/com/example/mdflow_recipes/ItemDetailFragment.java
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.mdflow_recipes.dummy.DummyContent;
@@ -25,7 +26,7 @@ public class ItemDetailFragment extends Fragment {
      * The fragment argument representing the item ID that this fragment
      * represents.
      */
-    String[] recipeTitles;
+    String[] recipeTitles, recipeIngredients, recipeProcedure;
     public static final String ARG_ITEM_ID = "item_id";
 
     /**
@@ -63,38 +64,52 @@ public class ItemDetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.item_detail, container, false);
         recipeTitles = getResources().getStringArray(R.array.recipeNames);
+        recipeIngredients = getResources().getStringArray(R.array.recipeIngredients);
+        recipeProcedure = getResources().getStringArray(R.array.recipeProcedure);
         // Show the dummy content as text in a TextView.
         if (mItem != null) {
             //((TextView) rootView.findViewById(R.id.item_detail)).setText(mItem.content);
             if (mItem.id.equals("1")){
                 rootView = inflater.inflate(R.layout.recipe_page, container, false);
-                ((TextView) rootView.findViewById(R.id.recipeTitle)).setText(mItem.recipeTitle);
-                ((TextView) rootView.findViewById(R.id.recipeDesc)).setText(mItem.recipeDescription);
+                ((TextView) rootView.findViewById(R.id.recipeTitle)).setText(recipeTitles[0]);
+                ((TextView) rootView.findViewById(R.id.recipeIngredients)).setText("Ingredients\n" + recipeIngredients[0]);
+                ((TextView) rootView.findViewById(R.id.recipeProcedure)).setText("Procedure\n" + recipeProcedure[0]);
+                ((ImageView) rootView.findViewById(R.id.recipePic)).setImageResource(R.drawable.bacon_spinach_stuffed_chicken);
             }
             if (mItem.id.equals("2")){
                 rootView = inflater.inflate(R.layout.recipe_page, container, false);
-                ((TextView) rootView.findViewById(R.id.recipeTitle)).setText(mItem.recipeTitle);
-                ((TextView) rootView.findViewById(R.id.recipeDesc)).setText(mItem.recipeDescription);
+                ((TextView) rootView.findViewById(R.id.recipeTitle)).setText(recipeTitles[1]);
+                ((TextView) rootView.findViewById(R.id.recipeIngredients)).setText("Ingredients\n" + recipeIngredients[1]);
+                ((TextView) rootView.findViewById(R.id.recipeProcedure)).setText("Procedure\n" + recipeProcedure[1]);
+                ((ImageView) rootView.findViewById(R.id.recipePic)).setImageResource(R.drawable.tex_mex_meatballs);
             }
             if (mItem.id.equals("3")){
                 rootView = inflater.inflate(R.layout.recipe_page, container, false);
-                ((TextView) rootView.findViewById(R.id.recipeTitle)).setText(mItem.recipeTitle);
-                ((TextView) rootView.findViewById(R.id.recipeDesc)).setText(mItem.recipeDescription);
+                ((TextView) rootView.findViewById(R.id.recipeTitle)).setText(recipeTitles[2]);
+                ((TextView) rootView.findViewById(R.id.recipeIngredients)).setText("Ingredients\n" + recipeIngredients[2]);
+                ((TextView) rootView.findViewById(R.id.recipeProcedure)).setText("Procedure\n" + recipeProcedure[2]);
+                ((ImageView) rootView.findViewById(R.id.recipePic)).setImageResource(R.drawable.calzone);
             }
             if (mItem.id.equals("4")){
                 rootView = inflater.inflate(R.layout.recipe_page, container, false);
-                ((TextView) rootView.findViewById(R.id.recipeTitle)).setText(mItem.recipeTitle);
-                ((TextView) rootView.findViewById(R.id.recipeDesc)).setText(mItem.recipeDescription);
+                ((TextView) rootView.findViewById(R.id.recipeTitle)).setText(recipeTitles[3]);
+                ((TextView) rootView.findViewById(R.id.recipeIngredients)).setText("Ingredients\n" + recipeIngredients[3]);
+                ((TextView) rootView.findViewById(R.id.recipeProcedure)).setText("Procedure\n" + recipeProcedure[3]);
+                ((ImageView) rootView.findViewById(R.id.recipePic)).setImageResource(R.drawable.spaghetti);
             }
             if (mItem.id.equals("5")){
                 rootView = inflater.inflate(R.layout.recipe_page, container, false);
-                ((TextView) rootView.findViewById(R.id.recipeTitle)).setText(mItem.recipeTitle);
-                ((TextView) rootView.findViewById(R.id.recipeDesc)).setText(mItem.recipeDescription);
+                ((TextView) rootView.findViewById(R.id.recipeTitle)).setText(recipeTitles[4]);
+                ((TextView) rootView.findViewById(R.id.recipeIngredients)).setText("Ingredients\n" + recipeIngredients[4]);
+                ((TextView) rootView.findViewById(R.id.recipeProcedure)).setText("Procedure\n" + recipeProcedure[4]);
+                ((ImageView) rootView.findViewById(R.id.recipePic)).setImageResource(R.drawable.beef_tacos);
             }
             if (mItem.id.equals("6")){
                 rootView = inflater.inflate(R.layout.recipe_page, container, false);
-                ((TextView) rootView.findViewById(R.id.recipeTitle)).setText(mItem.recipeTitle);
-                ((TextView) rootView.findViewById(R.id.recipeDesc)).setText(mItem.recipeDescription);
+                ((TextView) rootView.findViewById(R.id.recipeTitle)).setText(recipeTitles[5]);
+                ((TextView) rootView.findViewById(R.id.recipeIngredients)).setText("Ingredients\n" + recipeIngredients[5]);
+                ((TextView) rootView.findViewById(R.id.recipeProcedure)).setText("Procedure\n" + recipeProcedure[5]);
+                ((ImageView) rootView.findViewById(R.id.recipePic)).setImageResource(R.drawable.beef_totchos);
             }
         }
 
--- app/src/main/java/com/example/mdflow_recipes/dummy/DummyContent.java
+++ app/src/main/java/com/example/mdflow_recipes/dummy/DummyContent.java
@@ -27,12 +27,12 @@ public class DummyContent {
     //private static final int COUNT = 25;
 
     static {
-        addItem(new DummyItem("1", "Bacon & Spinach Stuffed Chicken", "- 4 boneless skinless chicken breasts\n- Kosher salt\n- Freshly ground black pepper\n- 4 oz. cream cheese, softened\n- 1/2 c. frozen spinach, defrosted and drained\n- 1/3 c. chopped artichoke hearts\n- 1 c. shredded mozzarella, dividedPinch crushed red pepper flakes\n- 4 strips bacon, cut into 4 strips\n- 2 tbsp. extra-virgin olive oil"));
-        addItem(new DummyItem("2", "Tex-Mex Meatballs", "- 1 1/2 lb. ground beef\n- 2 c. shredded Mexican cheese blend, divided\n- 1/2 c. panko bread crumbs\n- 2 tbsp. freshly chopped parsley, plus more for garnish\n- 2 cloves garlic, minced\n- 1 jalapeño, finely chopped\n- 1 large egg\n- 1 tsp. ground cumin\n- Kosher salt\n- Freshly ground black pepper\n- 1 tbsp. extra-virgin olive oil\n- 1/2 large onion, chopped\n- 1 (15-oz.) can crushed tomatoes\n- 2 tbsp. chopped chipotle chiles in adobo sauce"));
-        addItem(new DummyItem("3", "Best-Ever Calzones", "- Cooking spray\n- 1 3/4 lb. pizza dough\n- All-purpose flour, for surface\n- 1 c. pizza sauce\n- 1 c. ricotta\n- 1/2 c. pepperoni\n- 1 c. shredded mozzarella\n- Extra-virgin olive oil, for brushing\n- Kosher salt"));
-        addItem(new DummyItem("4", "Baked Spaghetti", "- Cooking spray\n- 16 oz. spaghetti\n- 2 tbsp. extra-virgin olive oil\n- 1 medium yellow onion, finely chopped\n- 2 cloves garlic, minced\n- 1 lb. ground beef\n- kosher salt\n- Freshly ground black pepper\n- 1 tsp. Italian seasoning\n- 1 (28-oz.) can crushed tomatoes\n- 1/2 c. basil, chopped\n- 1 c. shredded mozzarella\n- 1/4 c. freshly grated Parmesan\n- Chopped fresh parsley, for garnish"));
-        addItem(new DummyItem("5", "Cheesy Ground Beef Tacos","- 1 large onion, chopped\n- 1 lb. ground beef\n- 1 (15-oz.) can fire-roasted tomatoes\n- 1 (15-oz.) can black beans, drained and rinsed\n- 1 tbsp. taco seasoning\n- kosher salt\n- 2 c. shredded cheddar or pepper jack\n- 8 Small flour tortillas\n- Sliced green onions, for serving\n- Sour cream, for serving"));
-        addItem(new DummyItem("6", "Beef Totchos","- Cooking spray\n- 2 (1-lb.) bags frozen tater tots\n- 1 tbsp. extra-virgin olive oil\n- 1 lb. ground beef\n- 1 tbsp. taco seasoning\n- 1 c. cherry tomatoes, quartered\n- 1 c. black beans\n- 1 jalapeño, sliced\n- 1 c. shredded cheddar\n- 1 c. shredded Monterey Jack\n- Sour cream, for serving\n- Guacamole, for serving\n- Freshly chopped cilantro, for serving"));
+        addItem(new DummyItem("1", "Bacon & Spinach Stuffed Chicken"));
+        addItem(new DummyItem("2", "Tex-Mex Meatballs"));
+        addItem(new DummyItem("3", "Best-Ever Calzones"));
+        addItem(new DummyItem("4", "Baked Spaghetti"));
+        addItem(new DummyItem("5", "Cheesy Ground Beef Tacos"));
+        addItem(new DummyItem("6", "Beef Totchos"));
 
         // Add some sample items.
         //for (int i = 1; i <= COUNT; i++) {
